cf10ac64280cbdd05e7a600de2c636916dc25724
AndyLeeGT <andy1999job@gmail.com>
2021-09-26 08:52:29 -0400

    basic frontend completed

---
+++ front_end/login_page_structure.js
@@ -0,0 +1,7 @@
+document.getElementById("login_input").onsubmit = function(){submitFunction()};
+
+function submitFunction(){
+    alert("PHP login connection will perform once we connect database");
+}
+
+
---
+++ front_end/password_reset.js
@@ -0,0 +1,5 @@
+document.getElementById("reset input").onsubmit = function() {resetFunction()};
+
+function resetFunction(){
+    alert("send Email. //do it later once we implement database");
+}
